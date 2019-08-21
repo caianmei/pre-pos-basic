4291f12b9d2e662f61cc2246fb50aa7e9c820e25
caianmei <1610143991@qq.com>
2019-08-21 09:55:20 +0800

    sdjsd

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function find(collection, element) {
+  for (let index = 0; index < collection.length; index++) {
+    if (collection[index]  === element) {
+        return collection[index];
+    }
+    
+  }
   return '实现练习要求，并改写该行代码。';
 }
